2e896500e200e1571b38bfe35d3a674dd0ed72cd
jacobota <otajacob@yahoo.com>
2022-07-14 19:23:20 -0700

    Tested button

--- src/components/Pitchers.js
+++ src/components/Pitchers.js
@@ -2,6 +2,16 @@ import React, { Component } from 'react'
 import './Pitchers.css';
 
 export class Pitchers extends Component {
+  state = {
+    pitchers: 0
+  }
+
+  handleClick = () => {
+    let pitcherName = "John"
+    console.log(pitcherName)
+    console.log(this.state.pitchers)
+    this.setState({ pitchers: this.state.pitchers + 1 });
+  }
   render() {
     return (
       <>
@@ -9,14 +19,25 @@ export class Pitchers extends Component {
         <h1 id='header'>Pitcher View</h1>
       </div>
       <div className='d-flex justify-content-center pt-5 pb-5'>
-        <button type="button" className="btn btn-outline-dark" id='button'><a href="#" className='p-4' id='buttonText'>+ ADD PITCHER</a></button>
+        <button type="button" className="btn btn-outline-dark" id='button' onClick={ this.handleClick }><a href="#" className='p-4' id='buttonText'>+ ADD PITCHER</a></button>
       </div>
-      <div className='d-flex justify-content-center pt-3 pb-5'>
-        <h1 id='text'>Pitchers : None</h1>
+      <div className='d-flex justify-content-center pt-3'>
+        <span id='pitchtext'>{ this.showPitchers() }</span>
       </div>
+        <Container />
       </>
     )
   }
+
+  showPitchers() {
+    return this.state.pitchers === 0 ? 'Pitchers Tracked : None' : 'Pitchers Tracked : ' + this.state.pitchers
+  }
+}
+
+const Container = () => {
+  return (
+    <h1>hi</h1>
+  )
 }
 
 export default Pitchers
